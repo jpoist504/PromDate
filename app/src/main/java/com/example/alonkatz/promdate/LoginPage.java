3f25e93761cb2a1e5e0105d2bb95c413eee529fa
Alon Katz <alony.katz@gmail.com>
2018-05-07 21:44:17 -0700

    Implemented Firebase to project

--- app/src/main/java/com/example/alonkatz/promdate/LoginPage.java
+++ app/src/main/java/com/example/alonkatz/promdate/LoginPage.java
@@ -3,11 +3,22 @@ package com.example.alonkatz.promdate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class LoginPage extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
