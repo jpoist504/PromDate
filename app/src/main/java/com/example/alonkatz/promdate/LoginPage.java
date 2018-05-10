f17a36affd1f760ff268242bf56807560600e6e9
alonkatz23 <alony.katz@gmail.com>
2018-05-10 04:16:52 -0700

    updated login page

--- app/src/main/java/com/example/alonkatz/promdate/LoginPage.java
+++ app/src/main/java/com/example/alonkatz/promdate/LoginPage.java
@@ -1,11 +1,19 @@
 package com.example.alonkatz.promdate;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -24,6 +32,7 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        setupUI(findViewById(R.id.backdrop));
 
         ferrari = (BlurImageView) findViewById(R.id.background);
         ferrari.setBlur(2);
@@ -38,11 +47,43 @@ public class LoginPage extends AppCompatActivity {
 
     public void benButtonPressed(View view){
         startActivity(new Intent(getApplicationContext(), SwipingActivity.class));
+        //finish();
+
+
     }
 
     public void jpButtonPressed(View view){
 
         startActivity(new Intent(getApplicationContext(), MessageView.class));
+    }
+
+    public void hideSoftKeyboard() {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) this.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                this.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void setupUI(View view) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard();
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/alonkatz/promdate/SwipingActivity.java
+++ app/src/main/java/com/example/alonkatz/promdate/SwipingActivity.java
@@ -1,7 +1,11 @@
 package com.example.alonkatz.promdate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
 
 public class SwipingActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class SwipingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_swiping);
+
+//        Button btn=(Button) findViewById(R.id.btnok);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                finish();
+//            }
+//        });
+    }
+
+
+    public void goBack(View view){
+       finish();
+
     }
 }
