f09ea373ca193d97df94bd32ec68e153c1d3bee1
alonkatz23 <alony.katz@gmail.com>
2018-05-14 01:24:56 -0700

    Created User

--- app/src/main/java/com/example/alonkatz/promdate/RegisterActivity.java
+++ app/src/main/java/com/example/alonkatz/promdate/RegisterActivity.java
@@ -1,13 +1,104 @@
 package com.example.alonkatz.promdate;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        setupUI(findViewById(R.id.registerBackdrop));
+        mAuth = FirebaseAuth.getInstance();
     }
+
+    public void hideSoftKeyboard() {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) this.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(
+                this.getCurrentFocus().getWindowToken(), 0);
+    }
+
+    public void setupUI(View view) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard();
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
+    }
+
+    public void registerUser(View view){
+        String email = ((EditText)findViewById(R.id.createEmailTextField)).getText().toString();
+        String password = ((EditText)findViewById(R.id.createPasswordTextField)).getText().toString();
+        String confirmPassword = ((EditText)findViewById(R.id.confirmPasswordTextField)).getText().toString();
+
+        if(email.equals("")||password.equals("")||confirmPassword.equals(""))
+            Toast.makeText(this, "Please fill all the boxes", Toast.LENGTH_SHORT).show();
+        else {
+            if(password.equals(confirmPassword)){
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("Email Creation", "createUserWithEmail:success");
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    startActivity(new Intent(getApplicationContext(), SwipingActivity.class));
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Log.w("Email Creation", "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                    //updateUI(null);
+                                }
+
+                                // ...
+                            }
+                        });
+            }
+        }
+    }
+
+
+
+    public void goBack(View view){
+        finish();
+    }
+
+
 }
